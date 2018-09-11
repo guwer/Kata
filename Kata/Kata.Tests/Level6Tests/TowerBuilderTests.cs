3d8c6c41a208da5f4d8de969458ae945c5ace9aa
Krzysztof Siwek <guvver@gmail.com>
2018-09-11 16:43:20 +0200

    Add Tower Builder Kata level 6

---
+++ Kata/Kata.Tests/Level6Tests/TowerBuilderTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kata.Level6;
+using NUnit.Framework;
+
+namespace Kata.Tests.Level6Tests
+{
+    [TestFixture]
+    public class TowerBuilderTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(string.Join(",", new[] { "*" }), string.Join(",", SolutionsLevel6.TowerBuilder(1)));
+            Assert.AreEqual(string.Join(",", new[] { " * ", "***" }), string.Join(",", SolutionsLevel6.TowerBuilder(2)));
+            Assert.AreEqual(string.Join(",", new[] { "  *  ", " *** ", "*****" }), string.Join(",", SolutionsLevel6.TowerBuilder(3)));
+        }
+    }
+}
---
+++ Kata/Kata/SolutionsLevel6.cs
@@ -0,0 +1,18 @@
+﻿namespace Kata.Level6
+{
+    public class SolutionsLevel6
+    {
+        public static string[] TowerBuilder(int nFloors)
+        {
+            string[] floors = new string[nFloors];
+            for (var i = 1; i <= nFloors; i++)
+            {
+                var spaces = new string(' ', nFloors - i);
+                var stars = new string('*', (i * 2) - 1);
+                var floor = string.Concat(spaces, stars, spaces);
+                floors[i - 1] = floor;
+            }
+            return floors;
+        }
+    }
+}
